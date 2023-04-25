c91b66dc80604edc87a2c3c6539b4f9849adf1e6
fabioshelbyaulnurhilmi <biohilmi23@gmail.com>
2023-04-25 14:20:47 +0700

    first commit

---
+++ database/seeders/DetailPenyakitSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DetailPenyakit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DetailPenyakitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DetailPenyakit::create([
+
+        ]);
+    }
+}
